0bd694866227ad0c41954d8479a011bdde6de5ee
snamiki1212 <snamiki1212@gmail.com>
2018-03-29 19:53:26 +0900

    Add project

---
+++ lib/pipe_logger.ex
@@ -0,0 +1,18 @@
+defmodule PipeLogger do
+  @moduledoc """
+  Documentation for PipeLogger.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PipeLogger.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pipe_logger_test.exs
@@ -0,0 +1,8 @@
+defmodule PipeLoggerTest do
+  use ExUnit.Case
+  doctest PipeLogger
+
+  test "greets the world" do
+    assert PipeLogger.hello() == :world
+  end
+end
