8e333737a2e9dc2c8508b56e3f19ee519637f226
snamiki1212 <snamiki1212@gmail.com>
2018-03-30 11:23:54 +0900

    Add description at this library

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule PipeLogger.MixProject do
       version: "0.1.0",
       elixir: "~> 1.3",
       start_permanent: Mix.env() == :prod,
+      description: description(),
       package: package(),
       deps: deps()
     ]
@@ -22,14 +23,23 @@ defmodule PipeLogger.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.14", only: :dev},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 
+  defp description() do
+    "Logging via pipe, not stop."
+  end
+  
   defp package() do
     [
       maintainers: ["snamiki1212"],
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Shun Namiki"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/snamiki1212/pipe_logger"}
     ]
   end
 end
