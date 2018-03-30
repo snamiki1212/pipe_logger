45a9cbe5ec1276bf7aa2d0b504c0f9ee4e651384
snamiki1212 <snamiki1212@gmail.com>
2018-03-30 11:24:41 +0900

    Add pipe logger feature

--- lib/pipe_logger.ex
+++ lib/pipe_logger.ex
@@ -1,18 +1,26 @@
 defmodule PipeLogger do
+  require Logger
   @moduledoc """
   Documentation for PipeLogger.
   """
 
   @doc """
-  Hello world.
+  Pipe Logging.
 
   ## Examples
 
-      iex> PipeLogger.hello
-      :world
+      iex> [1, 2, 3] |> PipeLogger.debug("msg") |> Enum.map(&(&1))
+      [1, 2, 3] # LOGGING >> 11:13:32.253 [debug] "msg"[1, 2, 3]
 
   """
-  def hello do
-    :world
+  def debug(data, msg \\ "", metadata \\ [])
+  def debug(data, msg, metadata) when msg == "" do
+    Logger.debug(inspect(data), metadata)
+    data
   end
+  def debug(data, msg, metadata) do
+    Logger.debug(inspect(msg) <> inspect(data), metadata)
+    data
+  end
+
 end
--- test/pipe_logger_test.exs
+++ test/pipe_logger_test.exs
@@ -2,7 +2,28 @@ defmodule PipeLoggerTest do
   use ExUnit.Case
   doctest PipeLogger
 
-  test "greets the world" do
-    assert PipeLogger.hello() == :world
+  @test_data [1, "STRING", {1, 2}, %{1 => 2}, [1, 2]]
+  test "binary list with no argument" do
+    assert @test_data |> PipeLogger.debug() |> Enum.map(&(&1)) == @test_data
+  end
+
+  test "binary list with string msg" do
+    assert @test_data |> PipeLogger.debug("msg") |> Enum.map(&(&1)) == @test_data
+  end
+
+  test "binary list with binary msg" do
+    assert @test_data |> PipeLogger.debug(123) |> Enum.map(&(&1)) == @test_data
+  end
+
+  test "binary list with no msg and metadata" do
+    assert @test_data |> PipeLogger.debug("", label: 123) |> Enum.map(&(&1)) == @test_data
+  end
+
+  test "binary list with string msg and metadata" do
+    assert @test_data |> PipeLogger.debug("hoge", label: 123) |> Enum.map(&(&1)) == @test_data
+  end
+
+  test "binary list with binary msg and metadata" do
+    assert @test_data |> PipeLogger.debug(123, label: 123) |> Enum.map(&(&1)) == @test_data
   end
 end
